b6df710e1f2375e955ed66e94e0785a757b4e0d8
AHMET <59730181+ahmet-sa@users.noreply.github.com>
2022-10-02 12:16:45 +0300

    Add files via upload

---
+++ flashing led with mqtt broker and esp8266/led_yakma_mqtt.ino
@@ -0,0 +1,109 @@
+
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+
+const char* ssid = "Redmi Note 9S";
+const char* password = "ahmet123";
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+const char *topic = "LED";
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    String message;
+    for (int i = 0; i < length; i++) {
+        message = message + (char) payload[i];  // convert *byte to string
+    }
+    Serial.print(message);
+    if (message == "off") { digitalWrite(13, LOW); }   // LED on
+    if (message == "on") { digitalWrite(13, HIGH); } // LED off
+    Serial.println();
+    Serial.println("-----------------------");
+}
+
+
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(topic, "hello emqx");
+    client.subscribe(topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(BUILTIN_LED, OUTPUT);    
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+  client.publish(topic, "hello emqx");
+    client.subscribe(topic);
+  }
+}
